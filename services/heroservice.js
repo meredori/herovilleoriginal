11a62b74e7c1c42c8969e18de57cc558dbb81c34
Meredori <durzan@gmail.com>
2017-07-29 18:45:01 +1000

    fix up hero service

--- services/heroservice.js
+++ services/heroservice.js
@@ -1,7 +1,47 @@
 app.factory('heroes', function ($http,game,items) {
     heroes = {};
     heroes.jobs = [];
-    heroes.heroClass = [];
+        heroes.heroClass = [
+        {
+            id: 0,
+            name: "None"
+        },
+        {
+            id: 1,
+            name: "Labourer"
+        },
+        {
+            id: 2,
+            name: "Adventurer"
+        }
+    ]
+
+        heroes.jobs = [
+        {
+            id: 0,
+            name: "Gather",
+            current: 0,
+            limit: 100,
+            enabled: true,
+            description: "A Gathering hero will collect resources every second."
+        },
+        {
+            id: 1,
+            name: "Apothecary",
+            current: 0,
+            limit: 1,
+            enabled: false,
+            description: "An Apothecary will make potions."
+        },
+        {
+            id: 2,
+            name: "Smith",
+            current: 0,
+            limit: 1,
+            enabled: false,
+            description: "A Smith will produce weapons."
+        }
+    ]
 
 
     heroes.addHero = function (heroName) {
