d19d298465b25f9be7aad896c44e92c4962553f6
Mitchell Lett <mitchell.lett@adelphi.digital>
2019-06-27 17:24:32 +1000

    add port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+const port = process.env.PORT || 80;
+
 if (environment.production) {
   enableProdMode();
 }
