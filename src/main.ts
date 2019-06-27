eacfb71a5c3847e722745ac178b821a749a9e5cc
Meredori <durzan@gmail.com>
2019-06-28 00:24:57 +1000

    build prod config on build and remove unused "port"

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-const port = process.env.PORT || 80;
-
 if (environment.production) {
   enableProdMode();
 }
