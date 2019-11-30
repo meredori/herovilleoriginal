23b350c4cb5b99833e97b39b356c1e59907abe45
Meredori <durzan@gmail.com>
2019-11-30 18:05:27 +1000

    starting from scratch

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+      new HtmlWebpackPlugin({
+          title: 'Output Management',
+      }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+   module: {
+     rules: [
+       {
+         test: /\.css$/,
+         use: [
+           'style-loader',
+           'css-loader',
+         ],
+       },
+       {
+           test: /\.(png|svg|jpg|gif)$/,
+           use: [
+               'file-loader',
+           ]
+       },
+       {
+           test: /\.(woff|woff2|eot|ttf|otf)$/,
+           use: [
+               'file-loader',
+           ]
+       }
+     ],
+   },
+};
