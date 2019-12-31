e686962cd766ca8de729acff29e9bedbe5a812e7
Meredori <durzan@gmail.com>
2020-01-01 09:23:13 +1000

    allow buildings to level up and change cost, also display on the component.
    TODO: Add checking to see if can afford and also to subtract from the total

--- src/layouts/settingsPage.jsx
+++ src/layouts/settingsPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import BasicSettings from '../components/settings/basicSettings'
 
-export default class SettingsPage extends React.Component{
-    render() {
-        return <div>
-            <h1>Settings</h1>
-                <BasicSettings></BasicSettings>
-            </div>
-    }
+const SettingsPage = () => {
+    return <div>
+    <h1>Settings</h1>
+        <BasicSettings></BasicSettings>
+    </div>
 }
+
+export default SettingsPage;
--- src/stores/buildings/helper.js
+++ src/stores/buildings/helper.js
@@ -1,8 +1,16 @@
-const upgradebuilding = (currentBuilding) => {
+export const upgradebuilding = (currentBuilding) => {
+    currentBuilding.level++;
+    currentBuilding.cost = upgradeCost(currentBuilding.cost,currentBuilding.level,currentBuilding.multiplier);
     switch(currentBuilding.id){
         //Camp: Hero Building
         case 0: 
-            currentBuilding.level++;
-            currentBuilding.cost 
+            return currentBuilding;
+        default:
+            return currentBuilding;
     }
+}
+
+
+export const upgradeCost = (cost,level,multiplier = 2) => {
+    return (cost + Math.pow(level+1,multiplier));
 }
